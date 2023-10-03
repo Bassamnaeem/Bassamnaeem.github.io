eb2a2740279b85f9859fe8111954bd03b5ca3116
Bassam Naeem <bassamnaeem01@gmail.com>
2023-10-03 15:23:33 +0500

    fixed bugs

--- containers/Greetings.jsx
+++ containers/Greetings.jsx
@@ -60,7 +60,7 @@ const Greetings = () => {
                     style={{ width: "500px" }}
                   /> */}
                   <Image
-                    src="/img/image.png"
+                    src="/bassamnaeem.png"
                     width={600}
                     height={600}
                     alt="My Image"
